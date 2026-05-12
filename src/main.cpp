fadf5be257ca3d83e662da120500fcbf8dae2a11
Yvon Meunier <meunieryvon6@gmail.com>
2026-05-11 23:38:24 -0400

    feat : edited some comments and the indentation

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,23 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow*, int width, int height);
-void processInput(GLFWwindow*);
+void framebuffer_size_callback(GLFWwindow *, int width, int height);
+void processInput(GLFWwindow *);
 
-int main() {
+int main()
+{
     // GLFW configuration
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-   
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     // this creates the window object
-    GLFWwindow* window = glfwCreateWindow(800,600,"My First Window", NULL, NULL);
-    
-    if (window == NULL) {
+    GLFWwindow *window = glfwCreateWindow(800, 600, "My First Window", NULL, NULL);
+
+    if (window == NULL)
+    {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -24,26 +26,29 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
     // we tell OpenGL the size of your window
-    glViewport(0,0,800,600);
+    glViewport(0, 0, 800, 600);
 
     // we tell GLFW what callback we want to call when the window is resized
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // render loop
-    while(!glfwWindowShouldClose(window)) {
-        // input
+    while (!glfwWindowShouldClose(window))
+    {
+        // input handling
         processInput(window);
 
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         // glfw : swap buffers and poll events
+        // you have 2 buffers, one is getting edited by the render engine and the other one is displayed on the screen
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -52,14 +57,15 @@ int main() {
     return 0;
 }
 
-void processInput(GLFWwindow* window) {
+void processInput(GLFWwindow *window)
+{
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
     }
-    
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0,0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
